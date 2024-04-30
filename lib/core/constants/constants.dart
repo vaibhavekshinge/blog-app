7c61b1e40e625e78c8ab9cb3fe9ee51d000b3e63
vaibhavekshinge <vaibhavekshinge18@gmail.com>
2024-05-01 00:46:22 +0530

    upload project

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,11 @@
+class Constants {
+  static const List<String> topics = [
+    'Technology',
+    'Business',
+    'Programming',
+    'Entertainment',
+    'Sports'
+  ];
+
+  static const noConnectionErrorMessage = 'No internet connection!';
+}
---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://qprhbycehmnvqylmsshk.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwcmhieWNlaG1udnF5bG1zc2hrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQxNDIxOTUsImV4cCI6MjAyOTcxODE5NX0.Hxyu1TnUH1P3_S_2UWyFPaOGK7eeSz6Uz1UQV7eY9-g';
+}
